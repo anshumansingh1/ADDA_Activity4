3dc7c8058605b2b480e1c6bc696c0c3a97ace406
Anshuman Singh <74292568+anshumansingh1@users.noreply.github.com>
2024-04-18 16:51:13 +0530

    Minor code formatting

--- src/main/java/com/asp/app/App.java
+++ src/main/java/com/asp/app/App.java
@@ -4,10 +4,8 @@ package com.asp.app;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello from ASP!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from ASP!");
     }
 }
